66bcffa83a660df9bb96a7591e2c6fca6a5e316e
Cassiopeia <86bouledeneige@gmail.com>
2021-06-18 18:31:54 +0800

    added composables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">
-    Home
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading ...</div>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
+import PostList from '../components/PostList.vue';
+import getPosts from '../composables/getPosts';
+
 export default {
   name: 'Home',
+  components: { PostList },
+  setup() {
+    const { posts, error, load } = getPosts();
+    load();
+    return { posts, error };
+  },
 };
 </script>
