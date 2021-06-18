19ef8adbf79042672db8055317b8334d8c0cf8ab
Cassiopeia <86bouledeneige@gmail.com>
2021-06-18 15:06:22 +0800

    starter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from '@vue/reactivity';
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
