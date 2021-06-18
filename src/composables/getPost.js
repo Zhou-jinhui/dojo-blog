7d65ac84eb454c79362ddd74c1a3d847ff906d23
Cassiopeia <86bouledeneige@gmail.com>
2021-06-18 19:22:33 +0800

    added Spinner component

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,28 +1,28 @@
 import { ref } from 'vue';
 
-const getPosts = () => {
-  const posts = ref([]);
+const getPost = (id) => {
+  const post = ref(null);
   const error = ref(null);
 
   const load = async () => {
     try {
-      // Simulate delay
+      // simulate delay
       await new Promise((resolve) => {
         setTimeout(resolve, 2000);
       });
 
-      let data = await fetch('http://localhost:3000/posts');
+      let data = await fetch(`http://localhost:3000/posts/${id}`);
       if (!data.ok) {
-        throw Error('no data available');
+        throw Error('That post does not exist.');
       }
-      posts.value = await data.json();
+      post.value = await data.json();
     } catch (err) {
       error.value = err.message;
       console.log(error.value);
     }
   };
 
-  return { posts, error, load };
+  return { post, error, load };
 };
 
-export default getPosts;
+export default getPost;
